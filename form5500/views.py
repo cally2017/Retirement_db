a833f2e2d36eef23afe2d3db64a5ece00f1d34c1
Joshua Otten <43281244+jotten7137@users.noreply.github.com>
2023-04-13 13:57:01 -0400

    Tables Populated

--- form5500/views.py
+++ form5500/views.py
@@ -7,15 +7,22 @@ def charts(request):
     return render(request,'form5500/charts.html')
 
 def state(request):
-    return render(request,'form5500/state.html')
+    state_list = Form5500.objects.order_by('EIN')[:51]
+    return render(request,'form5500/state.html', {'state_list': state_list})
 
 def asset(request):
-    return render(request,'form5500/asset.html')
+    asset_list = Form5500.objects.order_by('EIN')[:51]
+    return render(request,'form5500/asset.html', {'asset_list': asset_list})
 
 def industry(request):
-    return render(request,'form5500/industry.html')
-
+    industry_list = Form5500.objects.order_by('EIN')[:51]
+    return render(request,'form5500/industry.html',{'industry_list': industry_list})
 
 #def chart_view(request):
 #   qs = Form5500.objects.all()[:10]
 #   data = pd.DataFrame(qs)
+
+# def get_queryset(self, request): # new
+#         query = self.request.GET.get("q")
+#         state_list = Form5500.objects.filter(Sponsor_State__contains = query)
+#         return render(request,'form5500/state.html', {'state_list': state_list})
