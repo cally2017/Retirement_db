0545d41f195470dfeb3b3cbfb7812253cd38b1ff
cally2017 <63662861+cally2017@users.noreply.github.com>
2023-04-19 17:31:16 -0400

    Add files via upload

--- form5500/utils.py
+++ form5500/utils.py
@@ -20,15 +20,23 @@ def get_plot(x1, y1, x2, y2, x3, y3):
 
     plot1.barh(x1, y1)
     plot1.set_title("Top 10 States by Plan count")
+    plot1.set_xlabel("Count")
     for index, value in enumerate(y1):
-        plot1.text(value, index,
-                value)
+        plot1.text(value, index, '{:,}'.format(value))
     plot2.barh(x2, y2)
     plot2.set_title("Top 10 States by Plan Asset")
+    plot2.set_xticklabels(['0B','1,000B','2,000B','3,000B','4,000B','5,000B'])
+    plot2.set_xlabel("Total Plan Asset ($)")
+    for index, value in enumerate(y2):
+        plot2.text(value, index, '${:,}'.format(round(value/1000000000,2))+"B")
     plot3.bar(x3, y3)
-    plot3.set_title("Industry by Plan Asset")
+    plot3.set_title("Industry by Plan Asset")    
+    for index, value in enumerate(y3):
+        plot3.text(index-0.5, value, '{:,}'.format(round(value/1000000000,1))+"B", fontsize=6)
     #plot3.rc('axes', labelsize=8)
     plot3.tick_params(axis='x', labelrotation=90, labelsize=6)
+    plot3.set_yticklabels(['0B','2,000B','4,000B','6,000B','8,000B','10,000B','12,000B'])
+    plot3.set_ylabel("Total Plan Asset ($)")
     
     plt.tight_layout()
     graph = get_graph()
